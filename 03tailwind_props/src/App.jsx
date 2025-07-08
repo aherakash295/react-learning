a87e6e0f1e40b430f2f08bc67391af49e8d80867
akashaher295 <akashaher295@gmail.com>
2025-07-09 00:11:14 +0530

    Added tailwind and learned the props

---
+++ 03tailwind_props/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+
+  return (
+    <>
+     <Card  companyName="Application Nexus" about="This is service based company"/>
+     <br />
+     <br />
+     <Card  companyName="Infosys" about="This is product based company"/>
+    </>
+  )
+}
+
+export default App
